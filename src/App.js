93bd9d4aa497f86dee0a597907b99ede5d5bc01a
Morten Bonderup <moab@eaaa.dk>
2022-12-16 14:55:02 +0100

    Version1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [vare1, setVare1] = useState("");
+  const [vare2, setVare2] = useState("");
+  const [varer, setVarer] = useState([]);
+
+ 
+  async function handleInput(vare, antal) {
+    let updatedList = [...varer];   
+    setVarer(updatedList.concat(`{"${vare}":"${antal}"}`));
+    }  
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const formData = {
+      vareliste: JSON.stringify(varer)
+    }
+    console.log(formData);
+
+    document.getElementById("output").innerHTML = JSON.parse(formData.vareliste);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form onSubmit={handleSubmit}>
+      <label>Vare 1&nbsp;<input name="vare1" type="number" value={vare1} onChange={e => { setVare1(e.target.value); handleInput(e.target.name, e.target.value); }}></input></label><br></br>
+      <label>Vare 2&nbsp;<input name="vare2" type="number" value={vare2} onChange={e => { setVare2(e.target.value); handleInput(e.target.name, e.target.value); }}></input></label><br></br>
+      <button>Send</button>
+      <div id="output"></div>
+    </form>
+    
   );
 }
 
