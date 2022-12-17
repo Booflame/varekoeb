efb28a6e868ebf2cdd95cc2298912f16b262acf5
Morten Bonderup <moab@eaaa.dk>
2022-12-17 11:22:48 +0100

    Lørdagsversion

--- src/App.js
+++ src/App.js
@@ -7,28 +7,54 @@ function App() {
   const [vare2, setVare2] = useState("");
   const [varer, setVarer] = useState([]);
 
- 
   async function handleInput(vare, antal) {
-    let updatedList = [...varer];   
-    setVarer(updatedList.concat(`{"${vare}":"${antal}"}`));
+    let updatedList = [...varer]; 
+    let total = 0;
+
+    for (const vare of updatedList) {
+      total = total + parseInt(vare.antal);
+    }
+
+    if (total + parseInt(antal) < 11) {
+
+    const index = updatedList.findIndex(vareX => vareX.varenavn === vare);  
+    
+    if (index === -1) {
+      setVarer(updatedList.concat({varenavn: `${vare}`, antal:`${antal}`}));
+    } else {
+      updatedList[index].antal = antal;
+      setVarer(updatedList);
+    } 
+
+    } else {
+      alert("Antal varer højere end 10")
+    }
+
     }  
 
   async function handleSubmit(e) {
     e.preventDefault();
+
     const formData = {
       vareliste: JSON.stringify(varer)
     }
-    console.log(formData);
-
-    document.getElementById("output").innerHTML = JSON.parse(formData.vareliste);
-  }
+    console.log(formData)
+  } 
 
   return (
-      <form onSubmit={handleSubmit}>
+      <form onClick={handleSubmit}> 
       <label>Vare 1&nbsp;<input name="vare1" type="number" value={vare1} onChange={e => { setVare1(e.target.value); handleInput(e.target.name, e.target.value); }}></input></label><br></br>
       <label>Vare 2&nbsp;<input name="vare2" type="number" value={vare2} onChange={e => { setVare2(e.target.value); handleInput(e.target.name, e.target.value); }}></input></label><br></br>
       <button>Send</button>
-      <div id="output"></div>
+      {varer.map(vare => {
+        return (
+          <div key={vare.varenavn}>
+            <h2>name: {vare.varenavn}</h2>
+            <h2>antal: {vare.antal}</h2>
+            <hr />
+          </div>
+        );
+      })}
     </form>
     
   );
